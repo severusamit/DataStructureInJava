2b8602ee817adfb2a6ac3a6a13c97cb30309c0bc
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:40:02 +0530

    Create BitwiseShiftOperator.java

---
+++ src/Basic/BitwiseShiftOperator.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class BitwiseShiftOperator
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=25;
+        System.out.println("Value of a : "+ a +"Value of b : " +b);
+        System.out.println("Value of a&b  : " +(a&b));
+        System.out.println("Value of a|b : "+(a|b));
+        System.out.println("Value of a^b : "+(a^b));
+        System.out.println("Value of a<<2 : "+(a<<2));
+        System.out.println("Value of a>>2 : "+(a>>2));
+    }
+}
