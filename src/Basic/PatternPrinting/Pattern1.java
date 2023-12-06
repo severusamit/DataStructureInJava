182b6b5569fab9f949f00216df359f18208e77f6
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:59:13 +0530

    Update Pattern1.java
    Explanation of For Loop

--- src/Basic/PatternPrinting/Pattern1.java
+++ src/Basic/PatternPrinting/Pattern1.java
@@ -23,3 +23,23 @@ public class Pattern1
         }
     }
 }
+
+/*
+Trying to step by step
+when i = 1
+print ->1
+j values is 1 and limit exceeded
+when i=2
+print ->1 2
+j values is 2 and loop terminates
+when i=3
+print -> 1 2 3
+j values is 3 and loop terminates
+when i =4
+print -> 1 2 3 4
+j values is 4 and loop terminates
+when i=5
+print -> 1 2 3 4 5
+j values is 5 and loop terminates as j<=i which is 6<=5
+
+ */
