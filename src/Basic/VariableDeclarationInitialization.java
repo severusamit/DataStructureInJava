a8b743e546247ff507436d0a27998530f9b5786c
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:35:53 +0530

    Create VariableDeclarationInitialization.java
    Switch Case Problems

---
+++ src/Basic/VariableDeclarationInitialization.java
@@ -0,0 +1,26 @@
+package Basic;
+
+
+
+
+public class VariableDeclarationInitialization {
+    public static void main(String args[])
+    {
+        char grade;
+        int roll;
+        float per;
+        double marks;
+        String name;
+        grade='A';
+        roll =105;
+        per =89.43f;
+        marks=673542;
+        name = "Amit and Aditya";
+        System.out.println("Name : "+name);
+        System.out.println("Roll : "+roll);
+        System.out.println("Marks: "+marks);
+        System.out.println("Percentage : "+per);
+        System.out.println("Grade :" +grade);
+    }
+}
+
