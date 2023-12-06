5554da7a002c6268ceee38641b91d0e8a51fd031
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:39:59 +0530

    Create AssignmentOperator.java

---
+++ src/Basic/AssignmentOperator.java
@@ -0,0 +1,12 @@
+package Basic;
+
+public class AssignmentOperator
+{
+    public static void main (String args[])
+    {
+        int a=5;
+        System.out.println("Value of a : " +a);
+        a*=10;
+        System.out.println("Value of a after assignment : " +a);
+    }
+}
