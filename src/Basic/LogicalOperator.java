c8f6cd7dacf64efca3f5d67a8522e5458b2261ba
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:39:37 +0530

    Create LogicalOperator.java

---
+++ src/Basic/LogicalOperator.java
@@ -0,0 +1,17 @@
+package Basic;
+
+public class LogicalOperator
+{
+    public static void main(String args[])
+    {
+        boolean a=true, b=false;
+        System.out.println("Value of a : " +a);
+        System.out.println("Value of b :" +b);
+        System.out.println("Value of a&&b is : "+(a&&b));
+        System.out.println("Value of a||b : " +(a||b));
+        System.out.println("Value of b||b : " +(b||b));
+        System.out.println("Value of !a : "+(!a));
+        System.out.println("Value of !b : "+(!b));
+
+    }
+}
