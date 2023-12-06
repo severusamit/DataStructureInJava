6ee90189a3dda2416b0ee839a116cf703b1e9312
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:51:15 +0530

    Nested Loop

---
+++ src/Basic/Loop/NestedLoop.java
@@ -0,0 +1,15 @@
+package Basic.Loop;
+
+public class NestedLoop
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=1;j<=3;j++)
+            {
+                System.out.println("Value of  i = " + i + " Value of j = " +j);
+            }
+        }
+    }
+}
