a37be674a9945d690ebde679f2a121824359f0bc
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:39:29 +0530

    Create ForLoop3.java

---
+++ src/Basic/Loop/ForLoop3.java
@@ -0,0 +1,13 @@
+package Basic.Loop;
+
+public class ForLoop3
+{
+    public static void main(String arga[])
+    {
+        int i,j;
+        for (i=1, j=20;i<=10;i++,j--)
+        {
+            System.out.println("Value of i =" + i + "Value of j = "+j);
+        }
+    }
+}
