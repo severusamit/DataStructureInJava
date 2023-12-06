78ef76acb8af3efa801193146f1d8c0af03a031c
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:39:32 +0530

    Create ForLoop2.java

---
+++ src/Basic/Loop/ForLoop2.java
@@ -0,0 +1,13 @@
+package Basic.Loop;
+
+public class ForLoop2
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        for( ;i<=10;)
+        {
+            System.out.print(+ i++ +" ");
+        }
+    }
+}
