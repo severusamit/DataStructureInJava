c0351442c25ad352a7e185e96eab0cbc790002d2
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:51:56 +0530

    Pattern (ForLoop)
    Problem Statement :
    Print this :
    1
    12
    123
    1234
    Upto n

---
+++ src/Basic/PatternPrinting/Pattern1.java
@@ -0,0 +1,25 @@
+package Basic.PatternPrinting;
+/*
+1
+12
+123
+1234
+ */
+import java.util.Scanner;
+
+public class Pattern1
+{
+    public static void main(String args[])
+    {
+        int i,j;
+
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
