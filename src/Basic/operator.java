2e4405668518668e5175d5c3205bfbb24e3b609b
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:36:53 +0530

    Create operator.java

---
+++ src/Basic/operator.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class operator
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=15;
+        System.out.println("Value of a : " +a);
+        System.out.println("Value of b : "+b);
+
+        System.out.println("Value of a<=b : "+(a<=b));
+        System.out.println("Value of a>=b : "+(a<=b));
+        System.out.println("Value of a<b  :"+(a<b));
+        System.out.println("Value of a>b  :"+(a>b));
+        System.out.println("Value of a!=b :"+(a!=b));
+
+    }
+}
