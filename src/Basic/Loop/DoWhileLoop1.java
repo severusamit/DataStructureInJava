5f3d01fa54eb0cb97b8174207866708c9606dfb4
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-06 23:50:51 +0530

    DoWhile Example

---
+++ src/Basic/Loop/DoWhileLoop1.java
@@ -0,0 +1,14 @@
+package Basic.Loop;
+
+public class DoWhileLoop1
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+}
