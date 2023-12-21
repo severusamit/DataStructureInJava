c5114f0a3e4c57f38fe343699a73e09cef6c4477
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-21 21:46:59 +0530

    This Operator

---
+++ src/Basic/FunctionOverloading/ThisOperator.java
@@ -0,0 +1,22 @@
+package Basic.FunctionOverloading;
+class aa6
+{
+    int a=10,b;
+    void get(int a)
+    {
+        b=this.a+a;
+    }
+    void put()
+    {
+        System.out.println("Value of b : "+b);
+    }
+}
+public class ThisOperator
+{
+    public static void main(String args[])
+    {
+        aa6 ob = new aa6();
+        ob.get(15);
+        ob.put();
+    }
+}
