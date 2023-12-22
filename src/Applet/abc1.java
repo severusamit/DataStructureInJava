09cd9ba9762990240a6b9b926d00d32bc44cbcbc
severusamit <87641622+severusamit@users.noreply.github.com>
2023-12-22 20:53:23 +0530

    AppletViewer Code

---
+++ src/Applet/abc1.java
@@ -0,0 +1,11 @@
+package Applet;
+import java.applet.*;
+import java.awt.*;
+// <Applet code ="abc1.class" width='600' height ='600' ></Applet>
+public class abc1
+{
+    public void paint(Graphics g)
+    {
+        g.drawString("This is my 1st program in Applet",300,200);
+    }
+}
