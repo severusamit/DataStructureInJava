6971e20ac37b7db5e00f2a2db72daab6d02a7fae
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-04 16:46:06 +0530

    Bus Fare

---
+++ src/Class/BusFare.java
@@ -0,0 +1,89 @@
+package Class;
+
+import java.util.Scanner;
+
+public class BusFare
+{
+    private String pName;
+    private int pAge;
+    private double pFare;
+    private char pGender;
+
+    void setName(String pName)
+    {
+       this.pName =pName;
+    }
+    String getName()
+    {
+        return pName;
+    }
+    void setAge(int pAge)
+    {
+        this.pAge =pAge;
+    }
+    int getAge()
+    {
+        return pAge;
+    }
+    void setFare(double pFare)
+    {
+        this.pFare = pFare;
+    }
+    double getFare()
+    {
+        return pFare;
+    }
+    void setGender(char pGender)
+    {
+        this.pGender=pGender;
+    }
+    char getGender()
+    {
+        return pGender;
+    }
+    double cal()
+    {
+        double d=0;
+        if(pAge>=60)
+        {
+            d=pFare*.30;
+        }
+        else if (pAge<=12)
+        {
+            d=pFare*.20;
+        }
+        else if(pGender =='F' || pGender =='f')
+        {
+            d=pFare*.10;
+        }
+        return d;
+    }
+}
+
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        BusFare ob = new BusFare();
+        System.out.println(" Enter Passenger Name  :  ");
+        ob.setName(in.nextLine());
+
+        System.out.println("Enter gender : ");
+        ob.setGender(in.nextLine().charAt(0));
+
+        System.out.println("enter age :");
+        ob.setAge(in.nextInt());
+
+        System.out.println("Enter base fare :");
+        ob.setFare(in.nextDouble());
+
+        System.out.println("--------------------------------");
+        System.out.println("Name       :" +ob.getName());
+        System.out.println("Gender     :" +ob.getGender());
+        System.out.println("Age        :" +ob.getAge());
+        System.out.println("Base fare  :" +ob.getFare());
+        System.out.println("Discount   :" +ob.cal());
+        System.out.println("Net Fare   :" +(ob.getFare()-ob.cal()));
+    }
+}
