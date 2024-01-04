348ae46fb79571b7e17e0ccc2e18ab18ff30a830
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-04 16:17:39 +0530

    Outer Class

---
+++ src/Class/Outer.java
@@ -0,0 +1,12 @@
+package Class;
+import java.util.Scanner;
+public class Outer
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        aa7Outer ob = new aa7Outer();
+        ob.get(in.nextInt(),in.nextInt());
+        ob.put();
+    }
+}
---
+++ src/Class/aa7Outer.java
@@ -0,0 +1,17 @@
+package Class;
+
+public class aa7Outer
+{
+    int a,b,c;
+    void get(int x,int y)
+    {
+        a=x;
+        b=y;
+    }
+    void put()
+    {
+        System.out.println("VAlue of a  : "+a);
+        System.out.println("Value of b : "+b);
+        System.out.println("Pro of two no :" +(a*b));
+    }
+}
