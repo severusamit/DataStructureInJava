7e3bedfdaa29a9fb1de07a974558b8d7b5b14674
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-09 22:39:46 +0530

    Student Use
    Classes and Objects

---
+++ src/CodingNinjas/classesandobjects/StudentUse.java
@@ -0,0 +1,22 @@
+package CodingNinjas.classesandobjects;
+
+import java.util.Scanner;
+
+public class StudentUse
+{
+//    Scanner in = new Scanner(System.in);
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        System.out.println(s1);   // address where object is stored
+        s1.name="Amit";
+        s1.rollNumber=10;
+        System.out.println(s1.name + " " +s1.rollNumber);
+
+        
+        Student s2 = new Student();
+        s2.name="Ankush";
+        s2.rollNumber=50;
+        System.out.println(s2.name + " " +s2.rollNumber);
+    }
+
+}
