0ffc26304d85d161ba7020b44a86d9d3d6d501d3
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-09 22:57:45 +0530

    Getters and Setters

--- src/CodingNinjas/classesandobjects/Student.java
+++ src/CodingNinjas/classesandobjects/Student.java
@@ -4,4 +4,15 @@ public class Student {
     String name;
     private int rollNumber;
 
+    public int getRollNumber()
+    {
+        return rollNumber;
+    }
+    public void setRollNumber(int num){
+        if(num<=0)
+        {
+            return;
+        }
+        rollNumber=num;
+    }
 }
