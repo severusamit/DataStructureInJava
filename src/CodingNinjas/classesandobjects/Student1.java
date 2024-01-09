a61f391ac6b84e83cc23d4d1462a65557774df7a
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-10 02:13:32 +0530

    Student1
    Class and Objects

---
+++ src/CodingNinjas/classesandobjects/Student1.java
@@ -0,0 +1,11 @@
+package CodingNinjas.classesandobjects;
+
+import java.util.Scanner;
+
+
+public class Student1{
+String name;
+int rollNumber;
+}
+
+
---
+++ src/CodingNinjas/classesandobjects/StudentUse1.java
@@ -0,0 +1,19 @@
+package CodingNinjas.classesandobjects;
+
+
+public class StudentUse1 {
+    public static void main(String[] args) {
+        //Scanner s= new Scanner(source)
+        Student1 s1 = new Student1();
+        Student1 s2 = new Student1();
+        System.out.println(s1);
+        s1.name = "Manisha";
+        s1.rollNumber = 10;
+        System.out.println(s1.name + " " + s1.rollNumber);
+
+        s2.name = "Ankush";
+        s2.rollNumber = 50;
+
+        System.out.println(s2.name + " " + s2.rollNumber);
+    }
+}
