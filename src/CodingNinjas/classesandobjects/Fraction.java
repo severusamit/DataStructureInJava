9ad0e4321e03f231f26a33506f63cb14ea715d82
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-11 00:43:11 +0530

    Fraction Problem
    Usage of Different Learning Techniques Like : This keyword, static function.

---
+++ src/CodingNinjas/classesandobjects/Fraction.java
@@ -0,0 +1,97 @@
+package CodingNinjas.classesandobjects;
+
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator,int denominator)
+    {
+        this.numerator=numerator;
+        this.denominator=denominator;
+        simplify();
+    }
+    public void setNumerator(int numerator)
+    {
+        this.numerator=numerator;
+        simplify();
+    }
+    public int getNumerator()
+    {
+        return numerator;
+    }
+    public void setDenominator(int denominator)
+    {
+        if(denominator!=0) {
+            this.denominator = denominator;
+            simplify();
+        }
+        else
+        {
+            System.out.println("Denominator can't be zero");
+        }
+    }
+    public int getDenominator()
+    {
+        return denominator;
+    }
+    public void simplify()
+    {
+        int gcd=1;
+        int smaller=Math.min(numerator,denominator);
+        for(int i=2;i<=smaller;i++)
+        {
+            if(numerator%i==0 && denominator%i==0)
+            {
+                gcd=i;
+            }
+        }
+        numerator=numerator/gcd;
+        denominator=denominator/gcd;
+    }
+    public void increment()
+    {
+        numerator=numerator+denominator;
+        simplify();
+    }
+
+    public void print()
+    {
+        System.out.println("Fraction  : " +numerator + " /" + denominator);
+    }
+
+    public void add(Fraction f2)
+    {
+        //First Fraction is the fraction on which function is callsed
+        //Second function is passed as arguments
+        this.numerator=this.numerator * f2.denominator +this.denominator * f2.numerator;
+        this.denominator=this.denominator * f2.denominator;
+        simplify();
+    }
+    public void subtract(Fraction f2)
+    {
+        /*
+        Suppose Fraction 1 is 2/3 and Fraction 2 is 3/4
+                         2*4 - 3* 3
+                       --------------
+                            3 * 4
+         */
+        this.numerator=this.numerator *f2.denominator - this.denominator*f2.numerator;
+        this.denominator=this.denominator*f2.denominator;
+       // System.out.println("Denominator"+this.denominator);
+        simplify();
+    }
+    public void multiply(Fraction f2)
+    {
+        this.numerator=this.numerator * f2.numerator  ;
+        this.denominator=this.denominator * f2.denominator;
+        simplify();
+    }
+    public static Fraction add(Fraction f1, Fraction f2)
+    {
+        int newNum = f1.numerator* f2.denominator + f1.denominator*f2.numerator;
+        int newDeno =f1.denominator*f2.denominator;
+        Fraction f3 = new Fraction(newNum,newDeno);
+        return f3;
+    }
+}
---
+++ src/CodingNinjas/classesandobjects/FractionUse.java
@@ -0,0 +1,63 @@
+package CodingNinjas.classesandobjects;
+import java.util.*;
+public class FractionUse
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        Fraction f1 = new Fraction(2,3);
+//        f1.print();
+//        f1.setNumerator(12);
+//        f1.getDenominator();
+//        f1.increment();
+//        f1.print();
+        Fraction f2 = new Fraction(3,4);
+//        f2.print();
+//        f1.add(f2);
+//        f1.print();
+//        f1.subtract(f2);
+//        f1.print();
+//        f1.multiply(f2);
+//        f1.print();
+//        Fraction f3 = Fraction.add(f1,f2);
+//        f3.print();
+        int choice;
+        do {
+            {
+                System.out.println("Menu");
+                System.out.println("1. Addition");
+                System.out.println("2. Subtraction");
+                System.out.println("3. Multiplication");
+                System.out.println("4. Exit");
+                System.out.println("Enter your choice :");
+
+                choice = in.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("You have Selected Addition Option");
+                        f1.add(f2);
+                        f1.print();
+                        break;
+                    case 2:
+                        System.out.println("You have selected Subtraction Option");
+                        f1.subtract(f2);
+                        f1.print();
+                        break;
+                    case 3:
+                        System.out.println("You have selected Multiplication Option");
+                        f1.multiply(f2);
+                        f1.print();
+                        break;
+                    case 4:
+                        System.out.println("Exiting the Program. GoodBye! ");
+                        break;
+                }
+
+            }
+
+        }while (choice != 4) ;
+        in.close();
+
+
+    }
+}
